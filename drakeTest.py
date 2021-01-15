735532ed6731bf27e3b6bb04e67b645d728fcadc
arshdog <46729073+arshdog@users.noreply.github.com>
2021-01-14 22:12:14 -0800

    Update drakeTest.py

--- drakeTest.py
+++ drakeTest.py
@@ -1,6 +1,6 @@
-print('hello world drake test')
+print('hello world its the iranian stallion/ Im talkin the persian excersion AKA Arshia ')
 
-print('try tongue but hole?')
+print('Ry needs to try tongue but hole?')
 
 print("This is code Drake's contribution")
 
